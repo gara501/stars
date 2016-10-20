4f0b21c25bec18bbfb346d5bc2d312014947e2dd
andres ramirez <gandres.ramirez@gmail.com>
2016-10-19 19:52:13 -0500

    Solar System planets

--- stars.js
+++ stars.js
@@ -4,12 +4,67 @@ var points = (function () {
   var containerHeight = window.innerHeight-20;
   var startInterval = document.querySelector('.start');
   var stopInterval = document.querySelector('.stop');
-  var createPoint = function() {
+  var generatedSolarSystemPlanets = false;
+  var createElement = function(elem) {
     var el = document.createElement('div');
-    el.className = "point";
+    el.className = elem;
     return el;
   };
 
+  var planets = [{
+    id: 1,
+    name: 'Mercurio',
+    texture: 'https://gara501.github.io/stars/textures/mercury.png',
+    scale: '0.1'
+  },
+                {
+    id: 2,
+    name: 'Venus',
+    texture: 'https://gara501.github.io/stars/textures/venus.jpg',
+    size: '0.4'
+  },
+                {
+    id: 3,
+    name: 'Tierra',
+    texture: 'https://gara501.github.io/stars/textures/tierra.jpg',
+    size: '0.5'
+  },
+                {
+    id: 4,
+    name: 'Marte',
+    texture: 'https://gara501.github.io/stars/textures/mars.jpg',
+    size: '0.3'
+  },
+                {
+    id: 5,
+    name: 'Jupiter',
+    texture: 'https://gara501.github.io/stars/textures/jupiter.jpg',
+    size: '0.9'
+  },
+                {
+    id: 6,
+    name: 'Saturno',
+    texture: 'https://gara501.github.io/stars/textures/saturn.jpg',
+    size: '0.8'
+  },
+                {
+    id: 7,
+    name: 'Urano',
+    texture: 'https://gara501.github.io/stars/textures/uranus.png',
+    size: '0.3'
+  },
+                {
+    id: 8,
+    name: 'Neptuno',
+    texture: 'https://gara501.github.io/stars/textures/neptune.jpg',
+    size: '0.3'
+  },
+                {
+    id: 9,
+    name: 'Pluton',
+    texture: 'https://gara501.github.io/stars/textures/pluto.jpg',
+    size: '0.1'
+  },];
   var colors = [{
                  id: 1,
                  name: 'Alnitak',
@@ -74,15 +129,19 @@ var points = (function () {
     var animation = 'fly';
     if (topCuad && widthCuad) {
      animation = 'flybl';
+     //animation = 'elipse';
     }
     if (topCuad && !widthCuad) {
      animation = 'flybr';
+      //animation = 'elipse';
     }
     if (!topCuad && widthCuad) {
      animation = 'flytr';
+      // animation = 'elipse';
     }
     if (!topCuad && !widthCuad) {
      animation = 'flytl';
+      // animation = 'elipse';
     }
     return animation;
   }
@@ -97,6 +156,7 @@ var points = (function () {
     point.addEventListener('mouseout', function() {
       var ulId = parseInt(point.dataset.id, 10) - 1;
       remarkEl[ulId].style.color = point.dataset.color;
+      point.classList.remove('elipse');
     });
   };
 
@@ -111,9 +171,36 @@ var points = (function () {
     remark(point);
   };
 
+  var putPlanet = function(planet) {
+    var leftPos = getRandomPosition(1, containerWidth);
+    var topPos = getRandomPosition(1, containerHeight);
+    planet.style.left = leftPos + "px";
+    planet.style.top = topPos + "px";
+
+    container.appendChild(planet);
+  }
+
+  var solarSystem = {
+    generateSolarSystem: function() {
+     if (!generatedSolarSystemPlanets) {
+      planets.map(function(planet) {
+       var planetL = createElement('planet planet-texture');
+       planetL.setAttribute('data-texture', planet.texture);
+       planetL.setAttribute('data-name', planet.name);
+        planetL.style.transform = 'scale(' + planet.size + ')'
+       planetL.classList.add(planet.name);
+       planetL.style.backgroundImage = 'url(' + planet.texture  + ')';
+       putPlanet(planetL);
+      });
+      }
+      generatedSolarSystemPlanets = true;
+    }
+
+  };
+
   var newPoint =  {
     generatePoint: function() {
-      var point = createPoint();
+      var point = createElement('point');
       var color = getRandomColor();
       var opacity = getRandomOpacity();
       point.style.backgroundColor = colors[color].color;
@@ -141,16 +228,22 @@ var points = (function () {
   };
 
   return {
-   newPoint: newPoint
+   newPoint: newPoint,
+   planets: solarSystem
   };
 
 })();
 
 var point = points.newPoint;
+var planets = points.planets;
 document.querySelector('.start').addEventListener('click', function() {
 point.generateSky();
 });
 
 document.querySelector('.stop').addEventListener('click', function() {
 point.stop();
+});
+
+document.querySelector('.solar').addEventListener('click', function() {
+planets.generateSolarSystem();
 });
